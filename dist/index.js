4b5169190129af7df4981738f18bb00ab76cb1c4
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-08-21 18:50:18 +0100

    add built files

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+exports.getAirfields = exports.getAircraft = void 0;
+var get_aircraft_1 = __importDefault(require("./get-aircraft"));
+exports.getAircraft = get_aircraft_1["default"];
+var get_airfields_1 = __importDefault(require("./get-airfields"));
+exports.getAirfields = get_airfields_1["default"];
+//# sourceMappingURL=index.js.map
