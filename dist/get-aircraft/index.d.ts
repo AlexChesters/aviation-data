2df630042a91da93b720c5433efe2a68ce6aa6c2
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-08-27 13:51:34 +0100

    don't commit dist/ to source control

--- dist/get-aircraft/index.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type Aircraft = {
-    name: string;
-    value: string;
-    icaoCode: string;
-};
-declare const _default: () => Promise<Aircraft[]>;
-export default _default;
--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import getAircraft, { Aircraft } from './get-aircraft';
-import getAirfields, { Airfield, Frequency, Runway } from './get-airfields';
-export { getAircraft, getAirfields, Airfield, Frequency, Runway, Aircraft };
